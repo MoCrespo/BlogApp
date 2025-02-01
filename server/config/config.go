e7df274738bb8c04c71dde22d97b4a3216cc4ffd
MoCrespo <mcrespo495@gmail.com>
2025-02-01 21:06:34 +0200

    config file to get database info for connection later

---
+++ server/config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+}
+
+func LoadConfig() (*Config, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Config{
+		DBHost:     os.Getenv("DB_HOST"),
+		DBPort:     os.Getenv("POSTGRES_PORT"),
+		DBUser:     os.Getenv("POSTGRES_USER"),
+		DBPassword: os.Getenv("POSTGRES_PASSWORD"),
+		DBName:     os.Getenv("POSTGRES_DB"),
+	}, nil
+}

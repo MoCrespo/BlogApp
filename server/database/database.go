a87696c7a8c240414e208281868c3225a1608937
MoCrespo <mcrespo495@gmail.com>
2025-02-01 21:23:53 +0200

    connect to database

---
+++ server/database/database.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"fmt"
+
+	"github.com/MoCrespo/BlogApp/server/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB(cfg *config.Config) error {
+	dsn := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable",
+		cfg.DBUser, cfg.DBPassword, cfg.DBHost, cfg.DBPort, cfg.DBName)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	DB = db
+	return nil
+}
+
+func GetDB() *gorm.DB {
+	return DB
+}

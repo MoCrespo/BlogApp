ddd93f23c769619dafc1403c6de6aabf87843768
MoCrespo <mcrespo495@gmail.com>
2025-02-01 21:25:01 +0200

    run database func to connnect to database

--- server/main.go
+++ server/main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
+	"github.com/MoCrespo/BlogApp/server/config"
+	"github.com/MoCrespo/BlogApp/server/database"
+	"github.com/MoCrespo/BlogApp/server/models"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		panic("Failed to load config: " + err.Error())
+	}
+
+	err = database.InitDB(cfg)
+	if err != nil {
+		panic("Failed to initialize database: " + err.Error())
+	}
+
+	db := database.GetDB()
+	err = db.AutoMigrate(&models.User{})
+	if err != nil {
+		panic("Migration failed: " + err.Error())
+	}
+
+	fmt.Println("Database connection and migration successful!")
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {

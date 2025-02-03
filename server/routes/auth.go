b75f95ad9c8ceaff46b2b64d0389ba7e2f6a1431
MoCrespo <mcrespo495@gmail.com>
2025-02-03 23:24:14 +0200

    implement profile routes

--- server/routes/auth.go
+++ server/routes/auth.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/MoCrespo/BlogApp/server/controllers"
+	"github.com/MoCrespo/BlogApp/server/middleware"
 
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
@@ -9,11 +10,18 @@ import (
 
 func AuthRoutes(app *fiber.App, db *gorm.DB) {
 	authController := controllers.NewAuthController(db)
+	profileController := controllers.NewProfileController(db)
 
 	auth := app.Group("/auth")
 	{
 		auth.Post("/register", authController.Register)
 		auth.Post("/login", authController.Login)
 
+	}
+
+	profile := app.Group("/profile", middleware.AuthRequired)
+	{
+		profile.Get("/", profileController.GetProfile)
+		profile.Put("/", profileController.UpdateProfile)
 	}
 }

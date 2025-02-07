d13becca462924236d47ca120d399912fbc788ff
MoCrespo <mcrespo495@gmail.com>
2025-02-07 17:09:54 +0200

    implement dark mode style in login page

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -26,19 +26,21 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
-        <h1 className="text-2xl font-bold mb-6 text-center"> Login</h1>
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <div className="bg-gray-800 p-8 rounded shadow-md w-full max-w-md">
+        <h1 className="text-2xl font-bold mb-6 text-center text-gray-100">
+          Login
+        </h1>
         {error && <p className="mb-4 text-red-500">{error}</p>}
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label className="block text-gray-700 mb-2" htmlFor="email">
+            <label className="block text-gray-300 mb-2" htmlFor="email">
               Email
             </label>
             <input
               type="email"
               id="email"
-              className="w-full p-2 border border-gray-300 rounded"
+              className="w-full p-2 bg-gray-700 border border-gray-600 rounded text-gray-100 focus:outline-none"
               placeholder="Email "
               value={email}
               onChange={(e) => setEmail(e.target.value)}
@@ -46,13 +48,13 @@ const Login: React.FC = () => {
             />
           </div>
           <div className="mb-6">
-            <label className="block text-gray-700 mb-2" htmlFor="password">
-              password
+            <label className="block text-gray-300 mb-2" htmlFor="password">
+              Password
             </label>
             <input
               type="password"
               id="password"
-              className="w-full p-2 border border-gray-300 rounded"
+              className="w-full p-2 bg-gray-700 border border-gray-600 rounded text-gray-100 focus:outline-none"
               placeholder="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
@@ -61,7 +63,7 @@ const Login: React.FC = () => {
           </div>
           <button
             type="submit"
-            className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600 transition-colors"
+            className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-700 transition-colors"
           >
             Login
           </button>

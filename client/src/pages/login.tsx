ca0d3728bd787019a6c2d47121431306d789c800
MoCrespo <mcrespo495@gmail.com>
2025-02-07 16:55:00 +0200

    implement login page

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,7 +1,72 @@
-const Login = () => {
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import api from '../api/api';
+
+const Login: React.FC = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const response = await api.post('auth/login', { email, password });
+      const { token } = response.data;
+
+      localStorage.setItem('token', token);
+
+      navigate('/posts');
+    } catch (err: any) {
+      console.error(err);
+
+      setError(err.response?.data?.error || 'Login Error');
+    }
+  };
+
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-3xl font-bold underline">Login page</h1>
+      <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
+        <h1 className="text-2xl font-bold mb-6 text-center"> Login</h1>
+        {error && <p className="mb-4 text-red-500">{error}</p>}
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label className="block text-gray-700 mb-2" htmlFor="email">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="w-full p-2 border border-gray-300 rounded"
+              placeholder="Email "
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-700 mb-2" htmlFor="password">
+              password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="w-full p-2 border border-gray-300 rounded"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600 transition-colors"
+          >
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
